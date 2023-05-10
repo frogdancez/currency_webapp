7164668b76e22fa8df7398d172cf62793173b60c
BaoandHai <bichc48@gmail.com>
2023-05-10 10:46:37 +0700

    update_1

--- backend/data.py
+++ backend/data.py
@@ -1,9 +1,10 @@
 interest_rates = {
-    'VND': 0.06, # VietNam
     'USD': 0.0525, # United States
-    'CHF': 0.015, # Thụy Sĩ
+    'CHF': 0.015,  # Thụy Sĩ
     'EUR': 0.0375, # Pháp
     'JPY': -0.001, # Nhật
     'AUD': 0.0385, # Úc
-    'GBP': 0.0425 # Anh
+    'GBP': 0.0425, # Anh
+    'CAD': 0.045,  # Canada
+    'NZD': 0.0525  # New Zealand
 }
